510b95b8fdcb7c8dccf4bce8067e35b39b511cd5
Stefan <kazantseff2k@gmail.com>
2023-11-25 15:41:12 +0300

    add tests for market.increasePosition()

--- test/MarketTest.t.sol
+++ test/MarketTest.t.sol
@@ -100,7 +100,82 @@ contract MarketTest is Test {
         public
         asAccount(trader)
     {
-        // In setUp Alice deposited 10_000 USDC, with utilizationPercentage being 80%, only 8000 USDC can be used
+        vm.expectRevert(NotEnoughLiquidity.selector);
+        market.openPosition(5e18, 2000e8, true);
+    }
+
+    function testIncreasePositionRevertsIfPositionNotOpen()
+        public
+        asAccount(trader)
+    {
+        vm.expectRevert(PositionNotOpen.selector);
+        market.increasePosition(1e18, 1000e8);
+    }
+
+    function testIncreasePositionIncreasesCollateralAndTransfersItFromUser()
+        public
+        asAccount(trader)
+    {
+        market.openPosition(1e18, 1000e8, true);
+        (uint256 collateral, , , , ) = market.userPosition(trader);
+        uint256 oldMarketBalance = usdc.balanceOf(address(market));
+        uint256 oldUserBalance = usdc.balanceOf(trader);
+        market.increasePosition(0, 1000e8);
+        (uint256 newCollateral, , , , ) = market.userPosition(trader);
+        uint256 newMarketBalance = usdc.balanceOf(address(market));
+        uint256 newUserBalance = usdc.balanceOf(trader);
+        assertEq(newCollateral, collateral + 1000e8);
+        assertEq(newMarketBalance, oldMarketBalance + 1000e8);
+        assertEq(newUserBalance, oldUserBalance - 1000e8);
+    }
+
+    function testIncreasePositionIncreasesSizeAndOpenInterest()
+        public
+        asAccount(trader)
+    {
+        market.openPosition(1e18, 1000e8, true);
+        (, uint256 size, , , ) = market.userPosition(trader);
+        uint256 openInterestUnderlying = market.openInterstInUnderlyingLong();
+        uint256 openInterestUSD = market.openInterestUSDLong();
+        market.increasePosition(1e18, 0);
+        (, uint256 newSize, , , ) = market.userPosition(trader);
+        uint256 newOpenInterestUnderlying = market
+            .openInterstInUnderlyingLong();
+        uint256 newOpenInterestUSD = market.openInterestUSDLong();
+        assertEq(newSize, size + 1e18);
+        assertEq(newOpenInterestUnderlying, openInterestUnderlying + 1e18);
+        assertEq(newOpenInterestUSD, openInterestUSD + 2000e18);
+    }
+
+    function testIncreasePositionRevertsIfNewPositionExceedsLeverage()
+        public
+        asAccount(trader)
+    {
+        // 2x leverage
+        market.openPosition(1e18, 1000e8, true);
+        vm.expectRevert(PositionExceedsMaxLeverage.selector);
+        market.increasePosition(7e18, 0);
+    }
+
+    function testIncreasePositionCalculatesAveragePriceCorrectly()
+        public
+        asAccount(trader)
+    {
+        // Open position at price of 2000$
+        market.openPosition(1e18, 1000e8, true);
+        // Now price decreased to 1500$
+        priceOracle.updateAnswer(1500e8);
+        // Increasing position by 1 ETH, new average price should be 1750$
+        market.increasePosition(1e18, 0);
+        (, , uint256 price, , ) = market.userPosition(trader);
+        assertEq(price, 1750e18);
+    }
+
+    function testIncreasePositionRevertsIfNotEnoughLiquidity()
+        public
+        asAccount(trader)
+    {
+        //addmod In setUp Alice deposited 10_000 USDC, with utilizationPercentage being 80%, only 8000 USDC can be used
         // Openning position of 2ETH with 1000 USDC as collateral, so 3000 USDC is taken from LP
         market.openPosition(2e18, 1000e8, true);
         vm.expectRevert(NotEnoughLiquidity.selector);
