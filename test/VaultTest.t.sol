e5a50fccef9f074eeea58d1dec274d7fcfdb6bbb
Stefan <kazantseff2k@gmail.com>
2024-02-27 12:44:07 +0300

    fix tests

--- test/VaultTest.t.sol
+++ test/VaultTest.t.sol
@@ -12,35 +12,35 @@ contract VaultTest is Test {
     }
 
     function testFirstDeposit() public {
-        _depositInVault(aliceDepositor, 10000e8);
-        assertEq(10000e8, vault.totalUnderlyingDeposited());
-        assertEq(10000e8, vault.totalSupply());
-        assertEq(10000e8, vault.balanceOf(aliceDepositor));
+        _depositInVault(aliceDepositor, 10000e6);
+        assertEq(10000e6, vault.totalUnderlyingDeposited());
+        assertEq(10000e6, vault.totalSupply());
+        assertEq(10000e6, vault.balanceOf(aliceDepositor));
         (uint256 userShares, , ) = vault.userToPosition(aliceDepositor);
-        assertEq(10000e8, userShares);
-        assertEq(10000e8, vault.totalShares());
+        assertEq(10000e6, userShares);
+        assertEq(10000e6, vault.totalShares());
     }
 
     function testNonFirstDeposit() public {
-        _depositInVault(aliceDepositor, 10000e8);
-        _depositInVault(bobDepositor, 3500e8);
-        assertEq(13500e8, vault.totalUnderlyingDeposited());
-        assertEq(13500e8, vault.totalSupply());
-        assertEq(3500e8, vault.balanceOf(bobDepositor));
+        _depositInVault(aliceDepositor, 10000e6);
+        _depositInVault(bobDepositor, 3500e6);
+        assertEq(13500e6, vault.totalUnderlyingDeposited());
+        assertEq(13500e6, vault.totalSupply());
+        assertEq(3500e6, vault.balanceOf(bobDepositor));
         (uint256 userShares, , ) = vault.userToPosition(bobDepositor);
-        assertEq(3500e8, userShares);
-        assertEq(13500e8, vault.totalShares());
+        assertEq(3500e6, userShares);
+        assertEq(13500e6, vault.totalShares());
     }
 
     function testRedeemHalfPosition() public {
-        _depositInVault(aliceDepositor, 5000e8);
+        _depositInVault(aliceDepositor, 5000e6);
         vm.prank(aliceDepositor);
-        vault.redeem(2500e8, aliceDepositor, aliceDepositor);
-        assertEq(2500e8, vault.totalUnderlyingDeposited());
-        assertEq(2500e8, vault.totalSupply());
-        assertEq(2500e8, vault.balanceOf(aliceDepositor));
+        vault.redeem(2500e6, aliceDepositor, aliceDepositor);
+        assertEq(2500e6, vault.totalUnderlyingDeposited());
+        assertEq(2500e6, vault.totalSupply());
+        assertEq(2500e6, vault.balanceOf(aliceDepositor));
         (uint256 userShares, , ) = vault.userToPosition(aliceDepositor);
-        assertEq(2500e8, userShares);
-        assertEq(2500e8, vault.totalShares());
+        assertEq(2500e6, userShares);
+        assertEq(2500e6, vault.totalShares());
     }
 }
